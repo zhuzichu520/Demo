04022badea5649e7637e6e4cd6cb069686cae961
zhuzichu520 <zhuzichu520@gmail.com>
2020-03-31 18:50:12 +0800

    update

--- app/src/main/kotlin/com/netease/nim/demo/ui/message/main/fragment/FragmentMessage.kt
+++ app/src/main/kotlin/com/netease/nim/demo/ui/message/main/fragment/FragmentMessage.kt
@@ -6,7 +6,9 @@ import com.netease.nim.demo.base.FragmentBase
 import com.netease.nim.demo.databinding.FragmentMeBinding
 import com.netease.nim.demo.ui.message.main.arg.ArgMessage
 import com.netease.nim.demo.ui.message.main.viewmodel.ViewModelMessage
+import com.netease.nimlib.sdk.msg.MsgService
 import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum
+import javax.inject.Inject
 
 class FragmentMessage : FragmentBase<FragmentMeBinding, ViewModelMessage, ArgMessage>() {
 
@@ -14,10 +16,18 @@ class FragmentMessage : FragmentBase<FragmentMeBinding, ViewModelMessage, ArgMes
 
     override fun setLayoutId(): Int = R.layout.fragment_message
 
+    private lateinit var sessionType: SessionTypeEnum
+
+    @Inject
+    lateinit var msgService: MsgService
+
+    override fun initArgs(arg: ArgMessage) {
+        super.initArgs(arg)
+        sessionType = SessionTypeEnum.typeOfValue(arg.sessionType)
+    }
 
     override fun initLazyData() {
         super.initLazyData()
-        val sessionType = SessionTypeEnum.typeOfValue(arg.sessionType)
         loadData(sessionType)
     }
 
@@ -30,6 +40,17 @@ class FragmentMessage : FragmentBase<FragmentMeBinding, ViewModelMessage, ArgMes
                 viewModel.loadTeamInfo()
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        msgService.setChattingAccount(arg.contactId, sessionType)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        msgService.setChattingAccount(MsgService.MSG_CHATTING_ACCOUNT_NONE, SessionTypeEnum.None)
     }
 
 }
